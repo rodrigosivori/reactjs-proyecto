7a2f80edf0e39af7c3f22f117f67831eae4e89c5
Rodrigo Sivori <rodrigosivoridev@gmail.com>
2022-08-29 17:27:16 -0300

    Agrego imagen carrito

--- src/componentes/CartWidget.jsx
+++ src/componentes/CartWidget.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import imagen from "./images/foto-logofondoblanco.jpg"
+import imagen from "./images/logocarrito.png"
 
 const Cartwidget = () => {
     return (
         <div>
             <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/"><img src={imagen} width="48" alt="Carrito de compras"></img></a>
+                <a className="nav-link active" aria-current="page" href="/"><img src={imagen} width="25" alt="Carrito de compras"></img></a>
             </li>
         </div>        
     )
