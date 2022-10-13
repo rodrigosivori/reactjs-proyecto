75010752132e1ff4510e0233b785a22d5d7c567a
Rodrigo Sivori <rodrigo.sivori@gmail.com>
2022-10-13 18:36:20 -0300

    corrijo ruta de img

--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -22,7 +22,7 @@ const ItemDetail = ({item}) =>{
     return(
         <div className='row justify-content-center col-md-5 py-3'>
             <div className='card p-0'>
-                <img src={item.foto} className='card-img-top pb-3' alt={item.producto} />
+                <img src={'/images/' + item.foto} className='card-img-top pb-3' alt={item.producto} />
                 <h4 className='card-title text-center'><b>{item.producto}</b></h4>
                 <h5 className='card-text text-center'>${item.precio}</h5>
                 <p className='text-center'>{item.descripcion}</p>
